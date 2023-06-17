557c876357065504768c14ad24c0035d505b2eba
Lalu423 <>
2023-06-17 14:30:28 -0400

    add models

--- models/Tag.js
+++ models/Tag.js
@@ -7,6 +7,29 @@ class Tag extends Model {}
 Tag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Product',
+        key: 'id',
+        unique: false
+      }
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Tag',
+        key: 'id',
+        unique: false
+      }
+    }
+
   },
   {
     sequelize,
